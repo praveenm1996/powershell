37ccb217823c0c1f7193363d79792b8729287a6e
praveen <prawink13@gmail.com>
2020-06-05 14:55:49 +0530

    second commit

--- powershell.ps1
+++ powershell.ps1
@@ -1,10 +1,1 @@
- Volume in drive C has no label.
- Volume Serial Number is 0624-114F
-
- Directory of C:\Users\pattalam\Desktop\myproject
-
-05-06-2020  14:43    <DIR>          .
-05-06-2020  14:43    <DIR>          ..
-05-06-2020  14:43                 0 powershell.ps1
-               1 File(s)              0 bytes
-               2 Dir(s)  60,967,923,712 bytes free
+get-process
